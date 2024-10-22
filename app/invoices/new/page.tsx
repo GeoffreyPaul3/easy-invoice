9035c2a5f11ab234d4e174ed094adda9f7ed3e31
GeoffreyPaul3 <geofreypaul40@gmail.com>
2024-10-22 07:49:28 +0200

    first commit

---
+++ app/invoices/new/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { createAction } from '@/app/actions'
+import SubmitButton from '@/components/SubmitButton'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { startTransition, SyntheticEvent, useState,   } from 'react'
+import Form from "next/form"
+
+
+export default function CreateInvoice() {
+    const [state, setState] = useState('ready')
+    async function handleOnSubmit(event: SyntheticEvent) {
+        event.preventDefault()
+        if (state === 'pending') return;
+        setState('pending')
+        const target = event.target as HTMLFormElement;
+        startTransition(async () => {
+              const formData = new FormData(target);
+        await createAction(formData)
+        })
+      
+    }
+
+  return (
+    <div className='flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12'>
+    <div className="flex justify-between">
+      <h1 className='text-3xl font-bold'>
+        Create Invoice
+      </h1>
+      </div>
+      <Form action={createAction} onSubmit={handleOnSubmit} className='grid gap-4 max-w-xs'>
+        <div>
+            <Label htmlFor='name' className='block mb-2 text-sm font-semibold'>Billing Name</Label>
+            <Input  id='name' name='name' type="text" />
+        </div>
+        <div>
+            <Label htmlFor='email' className='block mb-2 text-sm font-semibold'>Billing Email</Label>
+            <Input  id='email' name='email' type="email" />
+        </div>
+        <div>
+            <Label htmlFor='value' className='block mb-2 text-sm font-semibold'>Value</Label>
+            <Input  id='value' name='value' type="text" />
+        </div>
+        <div>
+            <Label htmlFor='description' className='block mb-2 text-sm font-semibold'>Description</Label>
+            <Textarea name="description" id="description"></Textarea>
+        </div>
+        <div>
+           <SubmitButton />
+        </div>
+      </Form>
+    </div>
+  )
+}
+
---
+++ components/SubmitButton.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import React from 'react'
+import { Button } from './ui/button'
+import { useFormStatus } from 'react-dom'
+import { LoaderCircle } from 'lucide-react'
+
+const SubmitButton = () => {
+    const { pending } = useFormStatus() 
+
+  return (
+    <Button className='relative w-full font-semibold'>
+        <span className={pending ? 'text-transparent' : ''}>Submit</span>
+        {pending && (
+            <span className='absolute flex items-center justify-center w-full h-full'>
+                <LoaderCircle className='animate-spin' />
+            </span>
+        )}
+    </Button>
+  )
+}
+
+export default SubmitButton
